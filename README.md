1b8e2f7d1b2930e74ed9503aeca2a5927ea42dca
José Moreno <jomoespe@gmail.com>
2015-05-12 00:08:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# First go examples
+
+Fowolling [How to Write Go Code](https://golang.org/doc/code.html)
+
---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jomoespe/go_samples/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("!OG olleH"))	
+}
