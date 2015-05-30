eb650c95d0486d64e08781e90491dd11e9a9b352
José Moreno <jomoespe@gmail.com>
2015-05-31 00:02:49 +0200

    Update main.go

--- dagox/main.go
+++ dagox/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"encoding/json"
 	"gopkg.in/fsnotify.v1"
 	"io/ioutil"
 	"log"
+	"flag"
 )
 
 // the WPC message structure
@@ -18,14 +20,38 @@ type WpcMessage struct {
 	Movement  string `json:"mov"`
 }
 
+// a lookup structure
+//    about JSON mapping: https://eager.io/blog/go-and-json/    OK!
+//                        http://stackoverflow.com/questions/23798283/golang-unmarshal-json     OK!
+//                        http://stackoverflow.com/questions/21268000/unmarshaling-nested-json-objects-in-golang          
+type Lookup struct {
+	IdExt        string            `json:"id_Ext"`
+	ContainerId  string            `json:"entryContainerID"`
+	Id           string            `json:"id"`
+	LookupType   string            `json:"coreAttribs[0].id"`
+//	Description  string            `json:"id_Ext"`
+//	Descriptions map[string]string `json:"id_Ext"`
+}
+
 // the WPC message types
 const typeLookup                string = "190"
 const typeReferenciaSuperEspaña string = "101"
 
 func main() {
+	printTitle()
+
+	// process command line arguments
+    inputArg := flag.String("input", "/var/tmp", "WPC input files directory. Default is /var/tmp")
+    helpArg  := flag.Bool("help", false, "show help")
+	flag.Parse()
+	if *helpArg {
+		printHelp();
+		return
+	}
+
 	//fmt.Printf("Hello %s!\n", Fullname("Gordon", "the Gopher"))
 	log.Println("Starting....")
-	startWatcher("/var/dabox/wpc")
+	startWatcher(*inputArg /*"/var/dabox/wpc"*/)
 	log.Println("Starded")
 }
 
@@ -45,7 +71,6 @@ func startWatcher(path string) {
 				log.Println("event: ", event)
 				if event.Op&fsnotify.Write == fsnotify.Write {
 					log.Println("Modified file: " + event.Name)
-
 					if wpcMsg, err := unmarshal(event.Name); err == nil {
 						process(wpcMsg)
 					}
@@ -64,8 +89,8 @@ func startWatcher(path string) {
 }
 
 func unmarshal(filename string) (ret WpcMessage, err error) {
+	wpcMsg   := &WpcMessage{}
 	dat, err := ioutil.ReadFile(filename)
-	wpcMsg := &WpcMessage{}
 	if err != nil {
 		log.Println("error reading file", err)
 		return *wpcMsg, err
@@ -74,15 +99,55 @@ func unmarshal(filename string) (ret WpcMessage, err error) {
 	return *wpcMsg, nil
 }
 
+func unmarshalLookup(wpcMessage WpcMessage) (Lookup, error) {
+	lookup := &Lookup{}
+	json.Unmarshal([]byte(wpcMessage.Message), &lookup)
+	return *lookup,nil
+}
+
 func process(wpcMessage WpcMessage) {
 	switch  wpcMessage.ObjType {
 	case typeLookup:
 		log.Println("Es una [Lookup]")
+		lookup, err := unmarshalLookup(wpcMessage)
+		if err == nil {
+			// processLookup
+			log.Printf("processed lookup.id: " + lookup.Id)
+			log.Printf("processed lookup.type: " + lookup.LookupType)
+		}
 	case typeReferenciaSuperEspaña:
 		log.Println("Es una [Referencia Super España]")
 	default:
 		log.Println("Es un tipo no manejado por la plataforma")
 	}
+}
+
+func printTitle() {
+	fmt.Println("                         ,---,                                   ");
+	fmt.Println("                      ,`--.' |                                   ");
+	fmt.Println("    ,---,             |   :  :    ,---,.                         ");
+	fmt.Println("  .'  .' `\\           |   |  '  ,'  .'  \\                      ");
+	fmt.Println(",---.'     \\          '   :  |,---.' .' |   ,---.               ");
+	fmt.Println("|   |  .`\\  |         ;   |.' |   |  |: |  '   ,'\\ ,--,  ,--,  ");                                                               
+	fmt.Println(":   : |  '  |  ,--.--.'---'   :   :  :  / /   /   ||'. \\/ .`|   ");
+	fmt.Println("|   ' '  ;  : /       \\       :   |    ; .   ; ,. :'  \\/  / ;  ");
+	fmt.Println("'   | ;  .  |.--.  .-. |      |   :     '    | |: : \\  \\.' /   ");
+	fmt.Println("|   | :  |  ' \\__\\/: . .      |   |   . |'   | .; :  \\  ;  ;  ");
+	fmt.Println("'   : | /  ;  ,\" .--.; |      '   :  '; ||   :    | / \\  \\  \\");
+	fmt.Println("|   | '` ,/  /  /  ,.  |      |   |  | ;  \\   \\  /./__;   ;  \\");
+	fmt.Println(";   :  .'   ;  :   .'   \\     |   :   /    `----' |   :/\\  \\ ;");
+	fmt.Println("|   ,.'     |  ,     .-./     |   | ,'            `---'  `--`    ");
+	fmt.Println("'---'        `--`---'         `----'                        \n\n");
+	fmt.Println("Da'box: The WPC - New CatalogBrowsing API integration tool.\n")
+}
+
+func printHelp() {
+	fmt.Println("Usage:")
+	fmt.Println("    dagox [-input=<path_to_repo>] [-help]\n")
+	fmt.Println("where:")
+	fmt.Println("    -input=<path_to_repo>: Set the input WPC files directory. Default value is /var/tmp.")
+	fmt.Println("                           The application mus have read permissions on the directory.")
+	fmt.Println("    -help: Show this message.")
 }
 
 func print(wpcMessage WpcMessage) {
@@ -97,3 +162,5 @@ func print(wpcMessage WpcMessage) {
 	//log.Printf("    wpcMessage.Message=",   wpcMessage.Message)
 	log.Println(" ______________________________________________________")
 }
+
+
